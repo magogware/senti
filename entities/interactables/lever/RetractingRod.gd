6fbe7a4fb9737b1a7a66989a892d93ba40b05e56
Alex Barilaro <alex@barilaro.me>
2022-04-03 10:53:20 +1000

    Lock lever when fully opened

--- entities/interactables/lever/RetractingRod.gd
+++ entities/interactables/lever/RetractingRod.gd
@@ -1,5 +1,7 @@
 extends RigidBody
 
+const RANGE_OF_MOTION: float = -PI/2
+
 var y_destination: Vector3 = Vector3(0,0,0)
 var fully_open: Transform
 var fully_closed: Transform
@@ -10,11 +12,13 @@ var holder: Spatial
 var avg_rotation: float = 0
 var prior_y_bases = []
 
+var rotation_percentage: float = 0
+
 func _ready():
 	# Calculate 90 degrees front and back (or whatever max opening angle is decided to be)
 	
 	fully_closed = global_transform
-	fully_open = fully_closed.rotated(fully_closed.basis.x, (-PI/2))
+	fully_open = fully_closed.rotated(fully_closed.basis.x, RANGE_OF_MOTION)
 	fully_open.origin = fully_closed.origin
 	
 	
@@ -36,15 +40,9 @@ func _physics_process(delta):
 			else:
 				v_y = global_transform.basis.y.rotated(fully_closed.basis.x, delta*(deg2rad(5)))
 		
-#		var open_to_holder: Vector3 = holder.global_transform.origin - fully_open.origin
-#		open_to_holder.x = 0
-#		open_to_holder = open_to_holder.normalized()
 		if v_y.dot(-fully_open.basis.z) > 0:
 			v_y = fully_open.basis.y
 #
-#		var closed_to_holder: Vector3 = holder.global_transform.origin - fully_closed.origin
-#		closed_to_holder.x = 0
-#		closed_to_holder = closed_to_holder.normalized()
 		if v_y.dot(fully_closed.basis.z) > 0:
 			v_y = fully_closed.basis.y
 		
@@ -60,6 +58,8 @@ func _physics_process(delta):
 		var v_y: Vector3
 		if global_transform.basis.y.angle_to(fully_closed.basis.y) <= delta*deg2rad(1):
 			v_y = fully_closed.basis.y
+		elif global_transform.basis.y.angle_to(fully_open.basis.y) <= delta*deg2rad(1):
+			v_y = fully_open.basis.y
 		else:
 			v_y = global_transform.basis.y.rotated(fully_closed.basis.x, delta*deg2rad(5))
 			
@@ -71,6 +71,8 @@ func _physics_process(delta):
 		global_transform.basis.x = v_x
 		global_transform.basis.y = v_y
 		global_transform.basis.z = v_z
+		
+	rotation_percentage = abs((global_transform.basis.y.angle_to(fully_closed.basis.y)) / RANGE_OF_MOTION)
 
 	if prior_y_bases.size()>0:
 		var prev_basis: Vector3 = prior_y_bases.back()
@@ -78,12 +80,12 @@ func _physics_process(delta):
 		var current_angle: float = global_transform.basis.y.angle_to(start.basis.y)
 
 		if prev_angle < current_angle:
-			var multiple: float = ceil(prev_angle/deg2rad(30)) * deg2rad(30)
+			var multiple: float = ceil(prev_angle/deg2rad(5)) * deg2rad(5)
 			if (prev_angle <= multiple and multiple <= current_angle):
 				print("passed an interval of pi/12")
 				# if so, play sound
 		elif current_angle < prev_angle:
-			var multiple: float = ceil(current_angle/deg2rad(30)) * deg2rad(30)
+			var multiple: float = ceil(current_angle/deg2rad(5)) * deg2rad(5)
 			if (current_angle <= multiple and multiple <= prev_angle) and multiple!=0:
 				print("passed an interval of pi/12: "+str(multiple)+", "+str(current_angle))
 		
