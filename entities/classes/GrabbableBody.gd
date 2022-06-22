154269e2535e5116224f840c708369f01b44d0c3
Alex Barilaro <alex@barilaro.me>
2022-06-22 15:44:08 +1000

    Add interaction signals

--- entities/classes/GrabbableBody.gd
+++ entities/classes/GrabbableBody.gd
@@ -1,19 +1,21 @@
 class_name GrabbableBody
 extends RigidBody
 
-export var can_interact: bool = false
+signal interaction_began
+
+export var interactable: bool = false
 var holder: Spatial
 
 func grabbed(grabber: Spatial):
 	holder = grabber
-	gravity_scale = 0
-	if can_interact:
+	mode = RigidBody.MODE_KINEMATIC
+	if interactable:
 		set_process_input(true)
 	Utils.set_grabbed(self)
 	
 func released():
 	holder = null
-	gravity_scale = 1
+	mode = RigidBody.MODE_RIGID
 	set_process_input(false)
 	Utils.set_released(self)
 
@@ -25,4 +27,5 @@ func _interact():
 
 func _input(event):
 	if event.is_action_pressed("interact"):
+		emit_signal("interaction_began")
 		_interact()
